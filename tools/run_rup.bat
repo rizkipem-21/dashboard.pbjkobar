11b240ed2ecb80168ad68a1cba5a92ba2337f600
rizkipem-21 <rizki.pem@gmail.com>
2026-05-18 08:16:48 +0700

    auto update pengadaan 18/05/2026  8:16:48,37

--- tools/run_rup.bat
+++ tools/run_rup.bat
@@ -42,15 +42,15 @@ echo UPDATE LAST-UPDATE >> tools\log_rup.txt
 echo !dd! !bulan! !yyyy! ^| !hh!.!mn! WIB > data\last-update-rup.txt
 
 :: 3. PROSES UPLOAD KE GITHUB
+:: FIX LOCK (Mencegah error Git nyangkut sebelum perintah Git apapun berjalan)
+del /f /q .git\index.lock >nul 2>&1
+
 echo GIT CONFIG >> tools\log_rup.txt
 git config user.name "rizkipem-21"
 git config user.email "rizki.pem@gmail.com"
 
 echo GIT STATUS >> tools\log_rup.txt
 git status >> tools\log_rup.txt 2>&1
-
-:: FIX LOCK (Mencegah error Git nyangkut)
-del /f /q .git\index.lock >nul 2>&1
 
 echo GIT ADD >> tools\log_rup.txt
 git add . >> tools\log_rup.txt 2>&1
@@ -63,8 +63,8 @@ git push origin main >> tools\log_rup.txt 2>&1
 
 echo PUSH STATUS: %ERRORLEVEL% >> tools\log_rup.txt
 
-echo ========================= >> tools\log_rup.txt
 echo SELESAI %date% %time% >> tools\log_rup.txt
+echo ========================= >> tools\log_rup.txt
 
 :: 4. NOTIFIKASI SELESAI (POP-UP ALWAYS ON TOP SELAMA 5 DETIK)
 mshta vbscript:Execute("CreateObject(""WScript.Shell"").Popup(""Proses update RUP Multi-Tahun telah SELESAI!"", 5, ""Update Selesai"", 4160)(window.close)")
